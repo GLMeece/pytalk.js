91624ec3203b84bb5faa3a5ff7696abab098b63f
tsim0 <tsim0@users.noreply.github.com>
2016-05-31 22:16:46 +0400

    Replace ES6 export

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,20 @@
-/**
- * 	@desc Parse JSON string to object, considering NaN values
- * 	@desc Namely, translating '__pytalk__PositiveInfinity' to Infinity
- * 	@desc '__pytalk__NegativeInfinity' to -Infinity
- * 	@desc '__pytalk__NaN' to NaN
- * 
- *  @param {string} serialized - JSON 
- */
-export function parseJSON(serialized) {
-	return JSON.parse(serialized, (key, value) => {
-		if (key !== 'value') {
+module.exports = {
+	parseJSON(serialized) {
+		return JSON.parse(serialized, (key, value) => {
+			if (key !== 'value') {
+				return value;
+			}
+
+			switch (value) {
+				case '__pytalk__PositiveInfinity':
+					return Infinity;
+				case '__pytalk__NegativeInfinity':
+					return -Infinity;
+				case '__pytalk__NaN':
+					return NaN;
+			}
+
 			return value;
-		}
-
-		switch (value) {
-			case '__pytalk__PositiveInfinity':
-				return Infinity;
-			case '__pytalk__NegativeInfinity':
-				return -Infinity;
-			case '__pytalk__NaN':
-				return NaN;
-		}
-
-		return value;
-	});
-}
+		});
+	}
+};
